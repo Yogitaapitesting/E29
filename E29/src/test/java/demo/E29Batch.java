9dbbce18c19a7119ba793daaf06cff74366c4c5b
A A <a@a.com>
2025-09-18 15:20:13 +0530

    initial commit

---
+++ E29/src/test/java/demo/E29Batch.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class E29Batch {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Everyone");
+		System.out.println("Hello Everyone");
+		System.out.println("Hello Everyone");
+		System.out.println("Hello Everyone");
+
+	}
+
+}
