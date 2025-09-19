8f16d2fa7ec1439b80266082a729e94959044369
A A <a@a.com>
2025-09-19 14:54:48 +0530

    virat commit

---
+++ E29/src/test/java/demo/Virat.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Virat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Virat hello");
+		System.out.println("Virat hello");
+		System.out.println("Virat hello");
+
+	}
+
+}
